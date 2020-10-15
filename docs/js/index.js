cfef6857bd348cabde06b3ceecd017367866118b
Daniel Moulson <dan24797@gmail.com>
2020-10-15 12:46:11 +0100

    smooth scroll and alternating page colours

---
+++ docs/js/index.js
@@ -0,0 +1,5 @@
+/*! jmpr-site v1.0.0 | (c) 2020  | ISC License | git+https://github.com/moulson/jmpr-site.git */
+$((function(){
+    $('#hi').css('opacity', 0).fadeTo("slow", 1);
+    $('#asd').css('opacity', 0).delay(1000).fadeTo("slow",1);
+}));
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+$(function(){
+    $('#hi').css('opacity', 0).fadeTo("slow", 1);
+    $('#asd').css('opacity', 0).delay(1000).fadeTo("slow",1);
+});
