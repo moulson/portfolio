6f508956ca41ae299296692f11eb39e1c9b32a66
Daniel Moulson <dan24797@gmail.com>
2020-10-16 15:11:33 +0100

    change to cards

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,3 +3,9 @@ $((function(){
     $('#hi').css('opacity', 0).fadeTo("slow", 1);
     $('#asd').css('opacity', 0).delay(1000).fadeTo("slow",1);
 }));
+
+function scroller(el){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $('#skills').offset().top
+    }, 200);
+}
