1f125a3e19fa55bff6fe6ae5f5a498e9ff7220b6
moulson <dan24797@gmail.com>
2021-05-05 11:23:22 +0100

    add gtm

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,4 @@
-/*! danmoulson.com v1.0.0 | (c) 2020  | ISC License | git+https://github.com/moulson/portfolio.git */
+/*! danmoulson.com v1.0.0 | (c) 2021  | ISC License | git+https://github.com/moulson/portfolio.git */
 $((function(){
     $('#hi').css('opacity', 0).fadeTo("slow", 1);
     $('#asd').css('opacity', 0).delay(1000).fadeTo("slow",1);
